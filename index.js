84336a2107bc3d67ad067c8b4f6379951a516279
Lonhaax <ytng976@gmail.com>
2024-10-14 14:51:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ app.all('/player/growid/login/validate', (req, res) => {
     const _token = req.body._token;
     const growId = req.body.growId;
     const password = req.body.password;
+    const game_version = req.body.game_version;
 
     const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
+        `_token=${_token}&growId=${growId}&password=${password}&game_version${game_version}`,
     ).toString('base64');
 
     res.send(
